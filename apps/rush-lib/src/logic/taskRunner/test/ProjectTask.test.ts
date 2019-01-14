0dd766b3673fa3adba5465de3b1c66748b461e15
Scott Hysom <ScottHysom@users.noreply.github.com>
2019-01-14 10:02:13 -0800

    Add initial ProjectTask unit test.

--- apps/rush-lib/src/logic/taskRunner/test/ProjectTask.test.ts
+++ apps/rush-lib/src/logic/taskRunner/test/ProjectTask.test.ts
@@ -1,7 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
-import { convertSlashesForWindows } from '../ProjectTask';
+import { resolve } from 'path';
+import { convertSlashesForWindows, ProjectTask } from '../ProjectTask';
+import { RushConfiguration } from '../../../api/RushConfiguration';
+import { RushConfigurationProject } from '../../../api/RushConfigurationProject';
+import { PackageChangeAnalyzer } from '../../PackageChangeAnalyzer';
+import { Interleaver, ITaskWriter } from '@microsoft/stream-collator';
 
 describe('convertSlashesForWindows()', () => {
   it('converted inputs', () => {
@@ -18,5 +23,29 @@ describe('convertSlashesForWindows()', () => {
     expect(convertSlashesForWindows('/blah\\bleep && /bloop')).toEqual('/blah\\bleep && /bloop');
     expect(convertSlashesForWindows('cmd.exe /c blah')).toEqual('cmd.exe /c blah');
     expect(convertSlashesForWindows('"/blah/bleep"')).toEqual('"/blah/bleep"');
+  });
+});
+
+describe('ProjectTask', () => {
+  describe('with simple \'my-task\' task', () => {
+    const rushFilename: string = resolve(__dirname, 'repo', 'rush.json');
+    const rushConfiguration: RushConfiguration = RushConfiguration.loadFromConfigurationFile(rushFilename);
+    const packageChangeAnalyzer: PackageChangeAnalyzer = new PackageChangeAnalyzer(rushConfiguration);
+
+    it('executes the task', () => {
+      const project: RushConfigurationProject = rushConfiguration.projectsByName.get('a')!;
+      const writer: ITaskWriter = Interleaver.registerTask('foo');
+      const projectTask: ProjectTask = new ProjectTask({
+        rushProject: project,
+        rushConfiguration,
+        commandToRun: 'my-task',
+        customParameterValues: [],
+        isIncrementalBuildAllowed: false,
+        ignoreMissingScript: false,
+        packageChangeAnalyzer
+      });
+      expect.assertions(1);
+      return expect(projectTask.execute(writer)).resolves.toEqual('SUCCESS');
+    });
   });
 });
